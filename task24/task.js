ac7844c85a0479330c8f5afe8a697ec3658abe15
wozhizuipc <wozhizui1990@163.com>
2016-09-08 10:45:15 +0800

    over task24
    功能实现over
    逻辑实现over
    代码略冗杂，后续有空重构

--- task24/task.js
+++ task24/task.js
@@ -1,9 +1,65 @@
+//全局变量区块
 var result = [];//用于存放遍历的dom节点
 var timer = null;//定义动画定时器
-var tip = null;
+//提示内容
+var tip = document.createElement('p');
+tip.setAttribute('class','tip');
+// 共享数据存放
+var rootNode = document.getElementById("wrapper");
+var selectdiv = null;
 
 window.onload = function(){
-  var rootNode = document.getElementById("wrapper");
+  // 为点选节点绑定事件
+  document.body.addEventListener('click',function(event){
+    if (event.target.tagName == "DIV") {
+      // 环境初始化
+      styleReset();
+      clearInterval(timer);
+      result = [];
+      // 为点选的div设定样式
+      selectdiv = event.target;
+      selectdiv.setAttribute('class','selectdiv');
+    }
+  });
+  // 为删除按钮绑定事件
+  document.getElementById('delit').onclick = function(){
+    // env init
+    styleReset();
+    clearInterval(timer);
+    result = [];
+    // checker
+    if (!selectdiv) {
+      tip.innerHTML = "Tip:未选定节点！";
+      document.body.appendChild(tip);
+      return false;
+    }
+    selectdiv.parentNode.removeChild(selectdiv);
+    selectdiv = null;
+  };
+  // 为增加按钮绑定事件
+  document.getElementById('pushit').onclick = function(){
+    // env init
+    styleReset();
+    clearInterval(timer);
+    result = [];
+    // checker
+    if (!selectdiv) {
+      tip.innerHTML = "Tip:未选定节点！";
+      document.body.appendChild(tip);
+      return false;
+    }
+    var addcontent = document.getElementById('addcontent').value;
+    if (!addcontent) {
+      tip.innerHTML = "Tip:请输入需要插入节点的内容！";
+      document.body.appendChild(tip);
+      return false;
+    }
+    // 正式函数体
+    var insertChild = document.createElement('div');
+    insertChild.textContent = addcontent;
+    selectdiv.appendChild(insertChild);
+    selectdiv = null;
+  }
   //为前序遍历按钮绑定事件
   document.getElementById("preOrder").onclick = function(){
     //环境初始化
@@ -34,14 +90,12 @@ window.onload = function(){
     postOrder(rootNode);
     startAnimate();
   }
-  //为查找操作绑定时间
+  //为查找操作绑定事件
   document.getElementById("findit").onclick = function(){
     // init
     styleReset();
     clearInterval(timer);
     result = [];
-    var tip = document.createElement('p');
-    tip.setAttribute('class','tip');
     // 前序遍历后得到result数组
     postOrder(rootNode);
     // get value后和数组进行比较
@@ -111,9 +165,8 @@ function startAnimate(){
 //查找动画
 function findAnimate(value){
   var i = 0;
-  var count =0;
-  var tip = document.createElement('p');
-  tip.setAttribute('class','tip');
+  var count =0;//为查找到的计数
+
   if(result[i].firstChild.textContent.trim().toLowerCase() == value.trim().toLowerCase()){
     result[i].setAttribute('class','markdiv');
     count++;
@@ -144,22 +197,37 @@ function findAnimate(value){
   }, 500);
 }
 
-//样式初始化函数
-function styleReset(){
+//样式初始化函数集合
+function clearBgColor() {
   var divEles = document.getElementsByTagName("div");
   for(var i=0; i<divEles.length;i++){
     divEles[i].style.backgroundColor = '#fff';
   }
-
+}
+function clearMark() {
   var markDivs = document.getElementsByClassName('markdiv');
   if (markDivs.length) {
     for (var j = 0; j < markDivs.length; j++) {
       markDivs[j].removeAttribute('class');
     }
   }
-
+}
+function clearTip() {
   var tips = document.getElementsByClassName('tip');
   if (tips.length) {
     tips[0].parentNode.removeChild(tips[0]);
   }
+}
+function clearSelected(){
+  var selected = document.getElementsByClassName('selectdiv');
+  if (selected.length) {
+    selected[0].removeAttribute('class');
+  }
+}
+// 总清理函数
+function styleReset(){
+  clearBgColor();
+  clearMark();
+  clearSelected();
+  clearTip();
 }
